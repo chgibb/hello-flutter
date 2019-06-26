695286c984d7e555c85bdee2f32d6ac273047fa2
chgibb <chris.gibb@outlook.com>
2019-06-26 12:25:04 -0400

    next step

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
 
-void main() => runApp(MyApp());
+import './pages/auth.dart';
+import './pages/products_admin.dart';
+import './pages/products.dart';
+import './pages/product.dart';
+
+void main() {
+  // debugPaintSizeEnabled = true;
+  // debugPaintBaselinesEnabled = true;
+  // debugPaintPointersEnabled = true;
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -10,44 +21,54 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<String> _products = [];
+  List<Map<String, dynamic>> _products = [];
+
+  void _addProduct(Map<String, dynamic> product) {
+    setState(() {
+      _products.add(product);
+    });
+    print(_products);
+  }
+
+  void _deleteProduct(int index) {
+    setState(() {
+      _products.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(title: Text("EasyList")),
-            body: Column(children: [
-              Container(
-                  margin: EdgeInsets.all(10.0),
-                  child: RaisedButton(
-                      onPressed: () {
-                        if (_products.length < 2) {
-                          this.setState(() {
-                            _products.add("item "+(_products.length + 1).toString());
-                          });
-                        }
-                      },
-                      child: Text("Add Product"))),
-              Column(
-                  children: _products.length > 0
-                      ? _products
-                          .map((el) => Card(
-                                  child: Column(
-                                children: <Widget>[
-                                  Image.asset("assets/food.jpg"),
-                                  Text(el)
-                                ],
-                              )))
-                          .toList()
-                      : []),
-              /*  Card(
-                  child: Column(
-                children: <Widget>[
-                  Image.asset("assets/food.jpg"),
-                  Text("Food Paradise")
-                ],
-              ))*/
-            ])));
+      // debugShowMaterialGrid: true,
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple),
+      // home: AuthPage(),
+      routes: {
+        '/': (BuildContext context) => AuthPage(),
+        '/products': (BuildContext context) => ProductsPage(_products),
+        '/admin': (BuildContext context) =>
+            ProductsAdminPage(_addProduct, _deleteProduct),
+      },
+      onGenerateRoute: (RouteSettings settings) {
+        final List<String> pathElements = settings.name.split('/');
+        if (pathElements[0] != '') {
+          return null;
+        }
+        if (pathElements[1] == 'product') {
+          final int index = int.parse(pathElements[2]);
+          return MaterialPageRoute<bool>(
+            builder: (BuildContext context) => ProductPage(
+                _products[index]['title'], _products[index]['image']),
+          );
+        }
+        return null;
+      },
+      onUnknownRoute: (RouteSettings settings) {
+        return MaterialPageRoute(
+            builder: (BuildContext context) => ProductsPage(_products));
+      },
+    );
   }
 }
